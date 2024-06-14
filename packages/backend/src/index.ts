b07e7094bec7305c5285bb27fec73bfc51ae2f1e
pstachula <pawel.stachula.mail@gmail.com>
2024-06-14 15:30:53 +0200

    ff

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { handle } from "@hono/node-server/vercel";
 import { cors } from "hono/cors";
 import { usersRoute } from "./routes/users";
 import { notesRoute } from "./routes/notes";
@@ -9,11 +8,9 @@ const app = new Hono()
 	.route("/users", usersRoute)
 	.route("/notes", notesRoute);
 
-// export default {
-// 	port: 2137,
-// 	fetch: app.fetch,
-// };
-
-export default handle(app);
+export default {
+	port: 2137,
+	fetch: app.fetch,
+};
 
 export type AppType = typeof app;
