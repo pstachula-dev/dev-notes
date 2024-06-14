750aa2c5fb779f6a7202ca85c9739d6261dc8594
pstachula <pawel.stachula.mail@gmail.com>
2024-06-14 14:57:39 +0200

    xxx

--- packages/backend/api/index.ts
+++ packages/backend/api/index.ts
@@ -1,14 +1,24 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
+import { usersRoute } from "../src/routes/users";
+import { notesRoute } from "../src//routes/notes";
 
 export const config = {
 	runtime: "edge",
 };
 
-const app = new Hono().basePath("/api");
+const app = new Hono()
+	.use(cors())
+	.basePath("/api")
+	.route("/users", usersRoute)
+	.route("/notes", notesRoute);
 
-app.get("/", (c) => {
-	return c.json({ message: "Hello Hono!" });
-});
+// export default {
+// 	port: 2137,
+// 	fetch: app.fetch,
+// };
 
 export default handle(app);
+
+export type AppType = typeof app;
